4fc831d580a027b4553d50a16e6f77c57eb0980c
Clemens Rudert <clemens.rudert@bl.ch>
2017-07-19 11:58:16 +0200

    add an additional test

--- tests/test_pyconizer.py
+++ tests/test_pyconizer.py
@@ -4,7 +4,13 @@ from pyconizer import create_icons_from_scratch
 
 
 def test_create_icons_from_scratch_json_only(test_path, test_config):
-
     path = os.path.abspath('{root}/json_only'.format(root=test_path))
     create_icons_from_scratch(test_config, path)
     assert os.path.isfile(os.path.abspath('{0}/mapping.json'.format(path)))
+
+
+def test_create_icons_from_scratch_json_and_icons(test_path, test_config):
+    path = os.path.abspath('{root}/json_and_icons'.format(root=test_path))
+    create_icons_from_scratch(test_config, path, json_only=False)
+    assert os.path.isfile(os.path.abspath('{0}/mapping.json'.format(path)))
+    assert os.path.isdir(os.path.abspath('{0}/ch.bav.kataster-belasteter-standorte-oev.oereb'.format(path)))
